7548d32ef4e25dfe43e108bf80619687b85c9426
David Vogel <david@truselo.com>
2025-11-15 11:03:52 -0800

    Test level with char movement

---
+++ char/player.gd
@@ -0,0 +1,29 @@
+extends CharacterBody3D
+class_name Player
+
+@export var Speed = 20.0
+@export var JumpVelocity = 4.5
+
+
+func _physics_process(delta: float) -> void:
+	var delta_speed = Speed * delta * Engine.physics_ticks_per_second
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JumpVelocity
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * delta_speed
+		velocity.z = direction.z * delta_speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, delta_speed)
+		velocity.z = move_toward(velocity.z, 0, delta_speed)
+
+	move_and_slide()
---
+++ char/slime.gd
@@ -0,0 +1,27 @@
+extends CharacterBody3D
+
+
+@export var Speed = 5.0
+
+var target: Player
+
+func _physics_process(delta: float) -> void:
+	var delta_speed = Speed * delta * Engine.physics_ticks_per_second
+	if target:
+		var direction = (target.global_position - global_position).normalized()
+		velocity.x = direction.x * delta_speed
+		velocity.z = direction.z * delta_speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, delta_speed)
+		velocity.z = move_toward(velocity.z, 0, delta_speed)
+	move_and_slide()
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		target = body as Player
+
+
+func _on_area_3d_body_exited(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		target = null
